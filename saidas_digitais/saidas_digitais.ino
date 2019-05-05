f402f37a83ec2b761fce34405e4e9c59b11652e2
sergio@peq.coppe.ufrj.br <sergio@peq.coppe.ufrj.br>
2019-05-05 12:10:54 -0300

    Update saidas_digitais.ino

--- saidas_digitais/saidas_digitais.ino
+++ saidas_digitais/saidas_digitais.ino
@@ -1,4 +1,4 @@
-//Delcaración de Los Pines de los LED
+//Delcaração dos LED
 byte led1=2;
 byte led2=3;
 byte led3=4;
@@ -8,12 +8,12 @@ byte led6=7;
 byte led7=8;
 byte led8=9;
 
-int i; //Variable del Contador del FOR
-long vel=100; //Velocidad de las Luces
+int i; //Contador para o FOR
+long vel=100; //Velocidade das Luzes
 
 void setup() {
   // put your setup code here, to run once:
-  //Configura los 8 Pines digitales como SALIDAS
+  //Configura os 8 Pinos digitais como SAIDAS
   for(i=led1;i<=led8;i++){
     pinMode(i,OUTPUT);
   }
@@ -21,30 +21,30 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- //Enciende los LED de Izquierda a Derecha
+ //Ascende os LEDs da esquierda para Direita
   for(i=led1;i<=led8;i++){
-    digitalWrite(i,HIGH); //Enciende el LED
-    delay(vel);           //Retardo (Espera el valor de vel)
+    digitalWrite(i,HIGH); //Ascende o LED
+    delay(vel);           //Atraso (Espera o valor de vel)
   }
 
-  //Apaga los LED de Derecha a Izquierda
+  //Desliga os LEDs de Direita para esquerda
   for(i=9;i>=2;i--){
-    digitalWrite(i,LOW); //Apaga el LED
-    delay(vel);          //Retardo (Espera el valor de vel)
+    digitalWrite(i,LOW); //Desliga o LED
+    delay(vel);          //Atraso (Espera o valor de vel)
   }
 
-  //Enciende los dos LEDs del Medio
+  //Ascende os dois LEDs do Meio
   digitalWrite(led4,1);
   digitalWrite(led5,1);
-  delay(1000);     //Retardo de 1000ms (1 segundo)
+  delay(1000);     //Atraso de 1000ms (1 segundo)
 
-  //Enciende los LEDs desde el MEDIO hacia los LADOS
+  //Ascende os LEDs desde o MEIO até os LADOS
   for(i=6;i<=8;i++){
     digitalWrite(i,LOW);
     digitalWrite(i+1,HIGH);
     digitalWrite(11-i,LOW);
     digitalWrite(10-i,HIGH);
-    delay(vel*2);  //Espera el Doble del valor de vel
+    delay(vel*2);  //aguarda o valor de vel veces 2
   }
   
 }
