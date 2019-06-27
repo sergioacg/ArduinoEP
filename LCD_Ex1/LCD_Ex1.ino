e80c16932af192ebdeb3c2138f2922a607dc1a5e
sergio@peq.coppe.ufrj.br <sergio@peq.coppe.ufrj.br>
2019-06-27 08:13:07 -0300

    Create LCD_Ex1.ino

---
+++ LCD_Ex1/LCD_Ex1.ino
@@ -0,0 +1,25 @@
+//Exemplo 1 - Utilização do Display LCD
+
+#include <LiquidCrystal.h>         
+ 
+LiquidCrystal lcd(10, 8, 5, 4, 3, 2); //(RS, E, D4,D5, D6, D7)
+ 
+void setup()
+{
+   lcd.begin(16, 2);   // Inicia o LCD 16x02 (colunas,linhas)   
+   lcd.setCursor(5, 0);   // Coloca o cursor nas coordenadas (5,0)   
+   lcd.print("Escola");   // Escreve no LCD   
+   lcd.setCursor(5, 1);   // Coloca o cursor nas coordenadas (5,1) 
+   lcd.print("Piloto");   // Escreve no LCD 
+}
+ 
+void loop()
+{
+   //Desligar o cursor
+   lcd.noCursor();
+   delay(500);
+   
+   //Ligar o cursor
+   lcd.cursor();
+   delay(500);
+}
