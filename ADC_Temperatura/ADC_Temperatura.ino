bdf7cb76b1404a9281dbb93093b61c4df5ac0b53
sergio@peq.coppe.ufrj.br <sergio@peq.coppe.ufrj.br>
2019-07-06 13:31:33 -0300

    Update ADC_Temperatura.ino

--- ADC_Temperatura/ADC_Temperatura.ino
+++ ADC_Temperatura/ADC_Temperatura.ino
@@ -15,24 +15,24 @@ LiquidCrystal lcd(10, 9, 8, 5, 4, 3, 2); //(RS, RW, E, D4,D5, D6, D7)
 void setup() {
  lcd.begin(16, 2);   // Inicia o LCD 16x02 (colunas,linhas)
  lcd.home();         // Coloca el cursor en las coordenadas (0,0)
- lcd.print("ADC Arduino");   // Escreve no LCD 
- lcd.setCursor(0,1);         // Coloca el cursor en las coordenadas (0,0)
- lcd.print("Temp: ");   // Escreve no LCD
+  lcd.print("ADC Arduino");   // Escreve no LCD 
+  lcd.setCursor(0,1);         // Coloca el cursor en las coordenadas (0,0)
+  lcd.print("Temp: ");   // Escreve no LCD  
 } 
  
 //Função que será executada continuamente
 void loop() {
+  
   //Leitura do Pino A0 (ADC)
   value=analogRead(LM35); // Armazena o valor inteiro.
   //Converte o valor inteiro em Temperatura
-  temperatura=(value*5/1023.0) / 0.01;
+  temperatura=(value*5/1023.0) / 0.010;
   
   //temperatura = (float(analogRead(LM35))*5/1023)/0.01;     //LM35
   //temperatura = (float(analogRead(LM35))*5/1023-0.5)/0.01; //TMP36
-  
-  lcd.setCursor(7,1);         // Coloca el cursor en las coordenadas (0,0)
+
+  lcd.setCursor(7,1);
   lcd.print(temperatura);   // Escreve no LCD
-  lcd.setCursor(14,1);
-  lcd.print("C");
+  lcd.print(" C");
   delay(1000);
 }
