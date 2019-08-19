71f344583d36d6cf14c4bec3f1673d17972d0bfe
sergio@peq.coppe.ufrj.br <sergio@peq.coppe.ufrj.br>
2019-08-19 20:06:05 -0300

    Create PWM_LED.ino

---
+++ PWM_LED/PWM_LED.ino
@@ -0,0 +1,12 @@
+#define LED 3 // PIN 13 as LED
+const int POT = A0; //Potenciometro
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(LED,analogRead(POT));
+}
