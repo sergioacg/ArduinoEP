95fa33de70c688d5c9345eabae747575cf42666d
sergio@peq.coppe.ufrj.br <sergio@peq.coppe.ufrj.br>
2019-08-25 21:36:11 -0300

    Update Teclado.ino

--- Teclado/Teclado.ino
+++ Teclado/Teclado.ino
@@ -1,11 +1,11 @@
 #include <Keypad.h>
 
-byte pinosLinhas[]  = {9,8,7,6};
+byte pinosLinhas[]  = {5,4,3,2};
 
-byte pinosColunas[] = {5,4,3,2};
+byte pinosColunas[] = {9,8,7,6};
 
-char teclas[4][4] = {{'1','2','3','A'},
-                     {'4','5','6','B'},
+char teclas[4][4] = {{'S','E','R','G'},
+                     {'I','O','6','B'},
                      {'7','8','9','C'},
                      {'*','0','#','D'}};
 
@@ -13,10 +13,10 @@ Keypad teclado1 = Keypad( makeKeymap(teclas), pinosLinhas, pinosColunas, 4, 4);
 
 void setup() {
   Serial.begin(9600);
+  delay(1000);
   Serial.println("Teclado 4x4 com Biblioteca Keypad");
   Serial.println("Aguardando acionamento das teclas...");
-  Serial.println();
-}
+  }
 
 void loop() {
   //Verifica se alguma tecla foi pressionada
